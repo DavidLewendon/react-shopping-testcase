2be91bbf186d6b13b2d24f615decebe36f0a3edb
David Lewendon <david@organic-web.net>
2020-05-13 21:35:16 +0100

    added page header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import './App.css';
-
+import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -14,6 +14,7 @@ const HatsPage =()=>(
 function App() {
   return (
     <div>
+      <Header />
       <switch>
       <Route exact path='/' component={Homepage} />
       <Route path='/shop' component={ShopPage} />
