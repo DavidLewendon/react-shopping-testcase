839f4d2864f916dd8add027b85c5d3c0175d3fa9
David Lewendon <david@organic-web.net>
2020-05-16 14:29:39 +0100

    added reselect to memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
-const CartDropDown = () => (
+const CartDropDown = ({cartItems}) => (
     <div className="cart-dropdown">
         <div className="cart-items">
-            
+            {cartItems.map(cartItem => (
+            <CartItem key={cartItems.id} item={cartItem} />
+            ))}
         </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartDropDown;
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
+})
+export default connect(mapStateToProps)(CartDropDown);
